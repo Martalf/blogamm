04ccc55d190380bf160635dc49f46e3aac101bc1
alfonso <martalf1987@gmail.com>
2019-08-24 20:57:55 -0400

    Blog version PC

---
+++ index.php
@@ -0,0 +1,483 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+	
+	<link rel="stylesheet" href="estilos/estilos.css" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Poppins|Righteous&display=swap" rel="stylesheet">
+	<meta charset="utf-8">
+	<title>My WebSite</title>
+
+	<!-- Inlcusion de Carrusel Owl-Carrusel -->
+
+	<link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+    <link rel="stylesheet" href="estilos/all.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+
+</head>
+
+<body>
+
+	<a id="whatsapp" href="https://wa.me/5804264042672"><img src="imagenes/Telefono50pixel.png" alt=""></a> <!-- esto es para el chat de WS hay probar su funcionamiento-->
+
+	<header>
+		
+		<section id="home">
+		
+			<div class="menu">	
+				<div class="contenedor" id="contenedor-menu">
+					
+					<p class="logo"><a href="#home">Martalf Dev</a></p>
+					
+					<nav>
+						<ul class="lista-menu">
+							
+							<li><a href="#sobremi">Sobre Mi</a></li>
+							<li><a href="#servicios">Servicios</a></li>
+							<li><a href="#misTrabajos">Mis Trabajos</a></li>
+							<li><a href="#trabajarConmigo">¿Por qué Trabajar conmigo?</a></li>
+							<li><a href="#miblog">Blog</a></li>
+							<li><a href="#contacto">Contactos</a></li>	
+						
+						</ul>
+					</nav>
+
+				</div>
+
+				<div class="contenedor">
+				
+					<div class="contenedor-titulo">
+					
+						<div class="titulo">
+							
+							<h1>Alfonso Martínez</h1>
+							<h2>Desarrollador TI</h2>
+							<a href="">Ver mis Trabajos</a>	
+
+						</div>
+					
+					</div>						
+				</div>
+			</div>
+		</section>
+	</header>	
+
+	<main>
+
+
+	<!-- :::::::::::::::::::::::::::::::::::::::: SOBRE MI ::::::::::::::::::::::::::::::::::::::: -->
+
+		<section id="sobremi">
+
+			<div class="contenedor">
+
+				<h3>Sobre Mi</h3>
+
+				<div class="contenido_sobremi">
+					
+					<div class="contenido_sobremi_imagen">
+						
+						<img src="imagenes/pc_escritorio.jpg" alt="">
+
+					</div>
+
+					<div class="contenido_sobremi_texto">
+						
+						<p>Ingeniero en el área de Informática con dos (2) años de experiencia en el desarrollo de aplicaciones para ambiente centralizado de IBM-AS400 atendiendo requerimientos de usuarios funcionales y remediando incidencias en procesos batch ejecutados por el sistema (procesos en segundo plano).</p>
+						
+						<p>Experiencia Básica e Intermedia en programación Web con lenguajes PHP, HTML, CSS, Javascript. Manejo de lenguaje de consulta de base de datos SQL. Conocimientos Básicos en el Framework-Backend Laravel y Framework-Frontend Angular.</p>
+						
+						<p>Capacidad de interpretación del idioma inglés a nivel técnico y fluidez básica para establecer conversaciones. Uso de los controladores de versiones GitHub y GitLab para la gestión controlada sobre proyectos de desarrollo y revision de Issues.</p>
+						
+						<p>Me caracterizo por ser una persona proactiva, ordenada, meticulosa, enfocada y orientada al logro de metas en corto, mediano y largo plazo. Los productos generados se basan en código optimizado, seguridad de la información y efectividad en la funcionalidad requerida por el cliente.</p>
+
+						<a href="">Ver mis Trabajos</a>
+
+					</div>
+
+				</div>
+
+			</div>
+			
+		</section>
+
+	<!-- :::::::::::::::::::::::::::::::::::: FIN SOBRE MI ::::::::::::::::::::::::::::::::::::::: -->
+	<!-- ::::::::::::::::::::::::::::::::::::: SERVICIOS ::::::::::::::::::::::::::::::::::::::::: -->	
+		<section id="servicios">
+			
+			<div class="contenedor">
+
+				<h3>Servicios</h3>
+
+				<div class="contenido_servicios">
+
+					<div class="servicio gris">
+						
+						<h4>APPS</h4>
+						
+						<p>Generación de software enfocado en requerimientos funcionales y no funcionales acordados entre el cliente y el desarrollador, considerando como premisas, la proteccion de los datos, la lógica de programacion y amigabilidad con los usuarios.</p>
+						
+						<img class ="icono" src="imagenes/CPUyCel(1).png" alt="" class="icono"><!-- icono o otro original-->
+
+						<img class ="ondas" src="imagenes/waves.png" alt=""> <!-- ondas incluir o una variacion original-->
+
+					</div>
+					
+					<div class="servicio azul">
+						
+						<h4>Base de Datos</h4>
+						
+						<p>Creación de Base de Datos ajustadas al modelo de negocio expuesto por el cliente enfocado en el almacenamiento de data 100% utilizable por la aplicación desarrollada.</p>
+						
+						<img class ="icono" src="imagenes/file(1).png" alt=""> <!-- icono o otro original-->
+
+						<img class ="ondas" src="imagenes/waves.png" alt=""> <!-- ondas incluir o una variacion original-->
+
+					</div>
+
+					<div class="servicio gris">
+						
+						<h4>SEO</h4>
+						
+						<p>Revisión de código fuente, análisis y adecuación de contenido sobre sitios web con el propósito de lograr un excelente posicionamiento en los buscadores de páginas en internet.</p>
+						
+						<img class ="icono" src="imagenes/Etadistica(1).png" alt=""><!-- icono o otro original-->
+
+						<img class ="ondas" src="imagenes/waves.png" alt=""> <!-- ondas incluir o una variacion original-->
+
+					</div>
+
+					<div class="servicio azul">
+						
+						<h4>Soporte Técnico</h4>
+						
+						<p>Diagnóstico y mantenimiento a equipos de cómputo y equipos periféricos, reparación de errores en Sistemas Operativos Windows y Linux, reinstalación de sistemas operativos, instalación y configuración de redes domésticas.</p>
+						
+						<img class ="icono" src="imagenes/Celular03.png" alt=""> <!-- icono o otro original-->
+
+						<img class ="ondas" src="imagenes/waves.png" alt=""> <!-- ondas incluir o una variacion original-->
+
+					</div>
+
+					<div class="servicio gris">
+						
+						<h4>Maquetación</h4>
+						
+						<p>Diseño de sitios  basados en el modelo de negocio del cliente, con una perspectiva vanguardista y agradable a la vista de los usuarios quienes querrán visitar con frecuencia su espacio en el internet.</p>
+						
+						<img class ="icono" src="imagenes/Lapiz(1).png" alt=""> <!-- icono o otro original-->
+
+						<img class ="ondas" src="imagenes/waves.png" alt=""> <!-- ondas incluir o una variacion original-->
+
+					</div>
+
+					<div class="servicio azul">
+						
+						<h4>Consultoría</h4>
+						
+						<p>Apoyo en el manejo de herramientas ofimáticas en los sistemas operativos Windows y Linux, asesoramiento en el uso de consolas bash sobre sistemas operativos previamente mencionados. Asesoramiento en la generación de diagramas UML para el diseño de software.</p>
+						
+						<img class ="icono" src="imagenes/Grupo.png" alt=""> <!-- icono o otro original-->
+
+						<img class ="ondas" src="imagenes/waves.png" alt=""> <!-- ondas incluir o una variacion original-->
+
+					</div>
+			
+				</div>
+				
+			</div>
+
+		</section>
+
+<!-- :::::::::::::::::::::::::::::::::::::: FIN SERVICIOS :::::::::::::::::::::::::::::::::::::: -->
+<!-- ::::::::::::::::::::::::::::::::::::::: MIS TRABAJOS :::::::::::::::::::::::::::::::::::::: -->
+
+
+		<section id="misTrabajos">
+
+			<div class="contenedor">
+
+				<h3 id="misTrabajos">Mis Trabajos</h3>
+				
+				<div class="owl-carousel owl-theme">
+
+      				<div class="item">
+      				<a href="https://www.google.com/">
+      					<img src="imagenes/CPUyCel(1)-Macbook Pro 2016.png">
+      					<p>Google</p>
+					</a>
+      				</div>
+
+      				<div class="item">
+      				<a href="https://www.google.com/">
+      					<img src="imagenes/CPUyCel(1)-Macbook Pro 2016.png">
+      					<p>Google</p>
+					</a>
+      				</div>
+
+      				<div class="item">
+      				<a href="https://www.google.com/">
+      					<img src="imagenes/CPUyCel(1)-Macbook Pro 2016.png">
+      					<p>Google</p>
+					</a>
+      				</div>
+
+      				<div class="item">
+      				<a href="https://www.google.com/">
+      					<img src="imagenes/CPUyCel(1)-Macbook Pro 2016.png">
+      					<p>Google</p>
+					</a>
+      				</div>
+
+      				<div class="item">
+      				<a href="https://www.google.com/">
+      					<img src="imagenes/CPUyCel(1)-Macbook Pro 2016.png">
+      					<p>Google</p>
+					</a>
+      				</div>
+
+      				<div class="item">
+      				<a href="https://www.google.com/">
+      					<img src="imagenes/CPUyCel(1)-Macbook Pro 2016.png">
+      					<p>Google</p>
+					</a>
+      				</div>
+
+					<div class="item">
+      				<a href="https://www.google.com/">
+      					<img src="imagenes/CPUyCel(1)-Macbook Pro 2016.png">
+      					<p>Google</p>
+					</a>
+      				</div>
+
+				</div>
+
+			</div>
+		
+		</section>
+
+<!-- ::::::::::::::::::::::::::::::::::::: FIN MIS TRABAJOS :::::::::::::::::::::::::::::::::::::: -->
+<!-- :::::::::::::::::::::::::::::::::::::: TRABAJAR CONMIGO ::::::::::::::::::::::::::::::::::::: -->
+
+	
+		<section id="trabajarConmigo">
+			
+			<div class="container">
+				
+				<h3>¿Por qué trabajar conmigo?</h3>
+
+				<div class="contenedorTrabajar">
+					
+					<div class="item">
+						
+						<ul>
+							<li><img src="imagenes/velocidad.png"></li>
+							<li>Planificación</li>
+							<li>Ejecución de proyectos de forma organizada mediante planes de trabajos que se ajustan a las necesidades del cliente, tomando en cuenta el cumplimiento de las fechas establecidas como marco de trabajo.</li>
+
+						</ul>
+
+					</div>
+
+				
+					<div class="item">
+						
+						<ul>
+							<li><img src="imagenes/velocidad.png"></li>
+							<li>Codificación</li>
+							<li>Desarrollo de APPS intuitivas y orientadas al dinanismo tomando como factor clave el tiempo de carga en los navegadores, la seguridad de la información y el diseño responsive hacia los diferentes dispositivos móviles existentes en la actualidad.</li>
+
+						</ul>
+
+					</div>
+				
+					
+					<div class="item">
+						
+						<ul>
+							<li><img src="imagenes/velocidad.png"></li>
+							<li>Asesoramiento</li>
+							<li>Orientación al cliente sobre el desarrollo del proyecto, e iteraciones consecutivas en función de mostrar los resultados en cada fase del desarrollo.</li>
+
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+<!-- :::::::::::::::::::::::::::::::: FIN TRABAJAR CONMIGO :::::::::::::::::::::::::::::::::::::: -->
+<!-- ::::::::::::::::::::::::::::::::::: INICIO MI BLOG ::::::::::::::::::::::::::::::::::::::::: -->
+
+
+		<section id = "miblog">
+			
+			<div class="contenedor">
+				
+				<h3>Blog</h3>
+
+				<div class="contenedor-publicaciones">
+					
+					<div class="publicacion">
+						
+						<div class="contenido">
+							<img src="imagenes/inthings.jpg">
+							<h4>¿Está en todo realmente?</h4>
+							<p>La tecnología avanza a un ritmo trepidante, no te quedes en el pasado, vive el presente y automatiza el futuro...</p>
+							<a href="">Leer Mas -></a>
+						</div>
+						
+					</div>
+
+					<div class="publicacion">
+						
+						<div class="contenido">
+							<img src="imagenes/inthings.jpg">
+							<h4>¿Está en todo realmente?</h4>
+							<p>La tecnología avanza a un ritmo trepidante, no te quedes en el pasado, vive el presente y automatiza el futuro...</p>
+							<a href="">Leer Mas -></a>
+						</div>
+	
+					</div>
+
+				</div>
+
+			</div>
+
+		</section>
+
+	<!-- ::::::::::::::::::::::::::::::::: FIN MI BLOG ::::::::::::::::::::::::::::::::::::::::: -->
+    <!-- :::::::::::::::::::::::::::::: INICIO QUE ESPERAS ::::::::::::::::::::::::::::::::::::: -->
+
+
+		<section id="que-esperas">
+			
+			<div class="contenedor">
+				
+				<h3>¿Qué esperas para comenzar a ofrecerte mis servicios?</h3>
+
+				<a href="">Conversemos</a>
+
+
+			</div>
+
+		</section>
+
+	<!-- ::::::::::::::::::::::::::::::::: FIN QUE ESPERAS ::::::::::::::::::::::::::::::::::: -->
+    <!-- ::::::::::::::::::::::::::::::::: INICIO CONTACTOS :::::::::::::::::::::::::::::::::: -->
+
+
+	<section id="contacto">
+			
+			<div class="contenedor">
+				
+				<h3>Contacto</h3>
+
+				<form action="formulario.php" method="POST" enctype="multipart/form-data">
+					
+					<input type="text" placeholder="Nombre" name="nombre">
+					<input type="email" placeholder="Email" name="email">
+					<textarea placeholder="Mensaje" name="mensaje"></textarea>
+					<input type="submit" value="Enviar">
+
+				</form>
+
+				<div class="contacto-info">
+					<div class="email"><img src="imagenes/mensaje.png" alt="">martalf1987@gmail.com</div>
+					<div class="ws"><img src="imagenes/Telefono.png" alt="">+5804264042672</div>
+				</div>
+
+			</div>
+
+	</section>		
+	
+   	</main>
+
+ <!-- ::::::::::::::::::::::::::::::::::: FIN DE CONTACTOS :::::::::::::::::::::::::::::::::: -->
+ <!-- ::::::::::::::::::::::::::::::::::: INICIO FOOTER    :::::::::::::::::::::::::::::::::: -->
+
+	<footer>
+		
+		<div class="contenedor">
+			
+			<div class="redes">
+				<a href="instagram.com/alfonsoorlandomartinez”"><img src="imagenes/insta.png" alt=""></a> <!-- para insta -->
+				<a href="www.linkedin.com/in/alfonso-martinez-morales-586858156"><img src="imagenes/linkedin.png" alt="logo linkedin"></a> <!-- para linkdin -->
+			</div>
+			<div class="parrafos">
+				
+				<p>2019 Alfonso Martínez | Desarrollador TI Venezuela. Todos los Derechos Reservados.</p>
+
+			</div>
+	
+		</div>
+
+	</footer>
+
+
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" 
+			integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+			crossorigin="anonymous"></script>
+	<script src="owl/owl.carousel.min.js"></script>
+
+	<script>
+		
+		$('.owl-carousel').owlCarousel({
+    		loop:true,
+    		margin:10,
+    		nav:true,
+    		dots: false,
+    		responsive:{
+        		0:{
+            		items:1
+        		},
+        		600:{
+            		items:3
+        		},
+        		1000:{
+            		items:4
+        		}
+    		}
+		})
+
+	</script>
+
+	<script>
+		
+	$(document).ready(()=>{
+		$(window).scroll(()=>{
+			scroll = $(window).scrollTop();
+				if(scroll > 100){ //cuando el scroll es hacia abajo
+					
+					$('#contenedor-menu.contenedor').css({"position":"fixed"});
+					$('#contenedor-menu.contenedor').css({"width":"100%"});
+					$('#contenedor-menu.contenedor').css({"top":"0"});
+					$('#contenedor-menu.contenedor').css({"background":"white"});
+					$('#contenedor-menu.contenedor').css({"z-index":"100"});
+					$('#contenedor-menu').css({"box-shadow":"rgba(0, 0, 0, 0.22) 1px 1px 1px"});
+					$('.lista-menu').css({"padding-right":"8%"});
+					$('.lista-menu a').css({"color":"black"});
+					$('.logo a').css({"color":"black"});
+					$('.logo').css({"padding-left":"5%"});	
+
+				}else{ //cuando el scroll es hacia arriba
+
+					$('#contenedor-menu.contenedor').css({"position":"relative"});
+					$('#contenedor-menu.contenedor').css({"background":"transparent"});
+					$('#contenedor-menu').css({"box-shadow":"0 0 0"});
+					$('.logo').css({"color":"white"});
+					$('.lista-menu a').css({"color":"white"});
+					$('.logo a').css({"color":"white"});
+				} 
+		})
+
+	})
+
+	</script>
+
+</body>	
+
+</html>
+
